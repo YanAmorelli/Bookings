816400030c61441c67c0d0af76b30edff57775a1
Yan L. Amorelli <yan.amorelli@trescon.com.br>
2022-01-17 10:18:00 -0300

    Creatingting repository

--- learning-go/cmd/web/main.go
+++ learning-go/cmd/web/main.go
@@ -15,18 +15,23 @@ const portNumber = ":8080"
 func main() {
 	var app config.AppConfig
 
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandler(repo)
+	
+	// Setting environment to dev 
+	app.UseCache = false
+	
+	// Setting environment to production
+	// app.UseCache = true
 	templateCache, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
 	}
-
 	app.TemplateCache = templateCache
 	render.NewTemplates(&app)
-
-	app.UseCache = false
-
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- learning-go/pkg/handlers/handlers.go
+++ learning-go/pkg/handlers/handlers.go
@@ -1,14 +1,31 @@
 package handlers
 
-import(
-	"github.com/YanAmorelli/learning-go/pkg/render"
+import (
 	"net/http"
+	"github.com/YanAmorelli/learning-go/pkg/config"
+	"github.com/YanAmorelli/learning-go/pkg/render"
 ) 
 
-func Home(w http.ResponseWriter, r *http.Request) {
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository{
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandler(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
